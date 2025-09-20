c91fb31caf9ad6566e92dfc9a27bd7892406053d
abhixyz1 <abhinayazrf@gmail.com>
2025-09-20 19:10:10 +0700

    completing function cekPrima for checking prime number with for iteration operation

--- week-3/prima.dart
+++ week-3/prima.dart
@@ -2,5 +2,12 @@ void main(List<String> args) {
 }
 
 bool cekPrima(int angka) {
+  if (angka < 2) return false;
+
+  for (int i = 2; i <= angka ~/ 2; i++) {
+    if (angka % i == 0) {
+      return false;
+    }
+  }
   return true;
 }
