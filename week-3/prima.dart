4ebf8e47ceb9bfb5ddf9351b4516db6b4b466c6c
abhixyz1 <abhinayazrf@gmail.com>
2025-09-20 11:06:52 +0700

    add operation for check prime number in cekPrima function

---
+++ week-3/prima.dart
@@ -0,0 +1,15 @@
+import 'dart:io';
+
+void main(List<String> args) {
+}
+
+bool cekPrima(int angka) {
+  if (angka < 2) return false;
+
+  for (int i = 2; i <= angka ~/ 2; i++) {
+    if (angka % i == 0) {
+      return false; 
+    }
+  }
+  return true;
+}
