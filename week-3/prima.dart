133ecd1bd38c9778e2f976a862b80182f6fd6d0c
abhixyz1 <abhinayazrf@gmail.com>
2025-09-20 11:21:02 +0700

    function main for run this program and add write for input the number

--- week-3/prima.dart
+++ week-3/prima.dart
@@ -1,6 +1,14 @@
 import 'dart:io';
 
 void main(List<String> args) {
+  stdout.write("Masukkan angka: ");
+  int angka = int.parse(stdin.readLineSync()!);
+
+  if (cekPrima(angka)) {
+    print("$angka adalah bilangan prima");
+  } else {
+    print("$angka bukan bilangan prima");
+  }
 }
 
 bool cekPrima(int angka) {
