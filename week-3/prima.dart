20fb844178c5a9335eb552b7c0ecc32ddb0b0bf4
abhixyz1 <abhinayazrf@gmail.com>
2025-09-20 22:45:52 +0700

    completing function main with write for input number

---
+++ week-3/prima.dart
@@ -0,0 +1,23 @@
+import 'dart:io';
+
+void main() {
+  stdout.write("Masukkan angka: ");
+  int angka = int.parse(stdin.readLineSync()!);
+
+  if (cekPrima(angka)) {
+    print("$angka adalah bilangan prima");
+  } else {
+    print("$angka bukan bilangan prima");
+  }
+}
+
+bool cekPrima(int angka) {
+  if (angka < 2) return false;
+
+  for (int i = 2; i <= angka ~/ 2; i++) {
+    if (angka % i == 0) {
+      return false;
+    }
+  }
+  return true;
+}
